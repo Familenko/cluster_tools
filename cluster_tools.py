b15bae858c99d9ace84adfccd099baecbe239417
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-01 21:15:08 +0300

    Update cluster_tools.py

--- cluster_tools.py
+++ cluster_tools.py
@@ -20,36 +20,6 @@ from sklearn.pipeline import make_pipeline
 
 class Clusterer():
 
-    '''
-    This class help to identify best amoun of clusters required for raw data to be divided for
-    For this purpose used Aglomerative clusterisation and required following step:
-
-        - present algorithm is very sensetive and required MinMax data preprocessing
-        wich can be done by method minmax
-
-        - main method is knife wich is building diagram wich includes silhuet coefficient for ich particular element of data
-        and represent it in different cluster. On diagram is also draph average silhuete score line. Respectly to this to
-        select correct amount of cluster it is required to visualy check silhuet of all 'knifes', they shape, length, leakege presents
-        and corresponding to average silhuete score line.
-
-        Best amount of cluster should show on diagram equal to ich other shape, length
-        of knifes without any leakega with the biggest amount of elements higher than average silhuete score line
-        Average silhuete score is close to 1.0
-
-        - next method is distance wich is building diagram wich is includes silhuet and distance_threshold,
-        both parameter required to be on maximum
-
-    To build required clusterisation used metod build_knife and build_simple (build on PCA preprocessed data)
-    Final dataframe posible to return by agglo.result or agglo.result_simple argument
-
-    It is posible to check data using PCA by method simple_check with different mode and parameters
-
-    Ordianry method for cluster assesment build informative diagram:
-
-        linkage - Check the optimal amount of cluster by scipy.cluster.hierarchy
-        dendrogram - By using this diagram possible make assesment of choosen amount of cluster on actual data
-        pie,heat,feature_corr - Build feature correlation diagram for 'knife' or 'simple' algorithm (use after build method)
-    '''
     def __init__(self,X):
         
         self.df = X
